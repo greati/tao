58957414974aeec2903c8efda0a60ce4ad27f715
Vitor Greati <greati@ufrn.edu.br>
2019-04-27 10:46:26 -0300

    Add Vec2

--- sources/include/tao/core.h
+++ sources/include/tao/core.h
@@ -18,5 +18,15 @@ using Vec3f = Mat<float, 3, 1>;
  * */
 using Vec3d = Mat<double, 3, 1>;
 
+/**
+ * A 3-dimensional column matrix of doubles.
+ * */
+using Vec2d = Mat<double, 2, 1>;
+
+/**
+ * A 3-dimensional column matrix of doubles.
+ * */
+using Vec2f = Mat<double, 2, 1>;
+
 }
 #endif
