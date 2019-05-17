28c78925cd5930d12073930100295abf31604849
Vitor Greati <greati@ufrn.edu.br>
2019-05-17 19:04:44 -0300

    Fix new dot product

--- sources/include/tao/linalg/Operations.h
+++ sources/include/tao/linalg/Operations.h
@@ -38,7 +38,7 @@ Mat<T, N, 1> unitize(const Mat<T, N, 1>& v1) {
  * */
 template<typename T, int N>
 T dot(const Mat<T, N, 1>& v1, const Mat<T, N, 1>& v2) {
-    T r;
+    T r { 0.0 };
     for (auto i {0}; i < N; ++i)
         r += v1(i) * v2(i);
     return r;
